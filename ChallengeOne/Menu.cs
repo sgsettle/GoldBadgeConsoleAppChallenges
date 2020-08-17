ab7079581bdafa8de1ef12fe5263f97511b940b8
sgsettle <s.settle41@gmail.com>
2020-08-17 11:28:51 -0400

    Added Challenge One First Commit

---
+++ ChallengeOne/Menu.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChallengeOne
+{
+    public class Menu
+    {
+        public Menu() { }
+
+        public Menu(int mealNumber, string mealName, string description, string ingredients, decimal price)
+        {
+            MealNumber = mealNumber;
+            MealName = mealName;
+            Description = description;
+            Ingredients = ingredients;
+            Price = price;
+        }
+
+        public int MealNumber { get; set; }
+        public string MealName { get; set; }
+        public string Description { get; set; }
+        public string Ingredients { get; set; }
+        public decimal Price { get; set; }
+    }
+}
---
+++ ChallengeOne/MenuRepository.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChallengeOne
+{
+    public class MenuRepository
+    {
+        private List<Menu> _menuDirectory = new List<Menu>();
+
+        // CREATE
+        public void AddContentToMenu(Menu content)
+        {
+            _menuDirectory.Add(content);
+        }
+
+        // READ
+        public List<Menu> GetMenu()
+        {
+            return _menuDirectory;
+        }
+
+        public Menu GetMenuItemByName(string mealName)
+        {
+            foreach (Menu item in _menuDirectory)
+            {
+                if (item.MealName.ToLower() == mealName.ToLower())
+                {
+                    return item;
+                }
+            }
+
+            return null;
+        }
+
+        // UPDATE
+        public bool UpdateExistingMenu(Menu updatedContent, string originalName)
+        {
+            Menu content = GetMenuItemByName(originalName);
+            if (content != null)
+            {
+                int itemIndex = _menuDirectory.IndexOf(content);
+                _menuDirectory[itemIndex] = updatedContent;
+                return true;
+            }
+
+            return false;
+        }
+
+        // DELETE
+        public bool DeleteExistingMenuItem(Menu content)
+        {
+            return _menuDirectory.Remove(content);
+        }
+
+    }
+}
